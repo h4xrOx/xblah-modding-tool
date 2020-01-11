200e1badb31011eae006f414e383673af17f0972
jeanknapp <email+bitbucket@jeankn.app>
2020-01-10 21:44:23 -0400

    Fixed error while parsing a mounted folder.

--- SourceSDK/MountedFolder.cs
+++ SourceSDK/MountedFolder.cs
@@ -12,7 +12,7 @@ namespace windows_source1ide
     {
         public MountedFolder(string fullPath, Steam sourceSDK) : base()
         {
-            this.fullPath = fullPath;
+            this.fullPath = fullPath.Replace("*", "");
             this.sourceSDK = sourceSDK;
 
             /*if (this.fullPath.EndsWith("."))
@@ -30,25 +30,24 @@ namespace windows_source1ide
 
             string packName = GetPackName();
 
-            foreach (string filePath in Directory.GetFiles(fullPath, "*", SearchOption.AllDirectories).Where(x => !x.EndsWith(".vpk")).ToArray())
-            {
-
-                string extension = new FileInfo(filePath).Extension;
-
-                Uri path1 = new Uri(fullPath + "\\");
-                Uri path2 = new Uri(filePath);
-                Uri diff = path1.MakeRelativeUri(path2);
-
-                File file = new File()
+            if (Directory.Exists(fullPath))
+                foreach (string filePath in Directory.GetFiles(fullPath, "*", SearchOption.AllDirectories).Where(x => !x.EndsWith(".vpk")).ToArray())
                 {
-                    path = diff.OriginalString.ToLower(),
-                    pack = packName,
-                    type = extension
-                };
-                files.Add(file.path, file);
-            }
 
-            
+                    string extension = new FileInfo(filePath).Extension;
+
+                    Uri path1 = new Uri(fullPath + "\\");
+                    Uri path2 = new Uri(filePath);
+                    Uri diff = path1.MakeRelativeUri(path2);
+
+                    File file = new File()
+                    {
+                        path = diff.OriginalString.ToLower(),
+                        pack = packName,
+                        type = extension
+                    };
+                    files.Add(file.path, file);
+                }
         }
 
         public override void extractFile(string filePath, string startupPath)
