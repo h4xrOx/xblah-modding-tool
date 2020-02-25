a41039f403169a041fdeff900be9de11e6a3e697
jeanknapp <email+bitbucket@jeankn.app>
2020-02-25 17:13:26 -0400

    Helper gameinfo class created.

---
+++ SourceSDK/GameInfo.cs
@@ -0,0 +1,28 @@
+﻿using SourceModdingTool.SourceSDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceModdingTool
+{
+    class GameInfo
+    {
+        Steam sourceSDK;
+
+        KeyValue root;
+
+        public GameInfo(Steam sourceSDK)
+        {
+            this.sourceSDK = sourceSDK;
+
+            root = KeyValue.readChunkfile(sourceSDK.GetModPath() + "\\gameinfo.txt");
+        }
+
+        public string getValue(string key)
+        {
+            return root.getValue(key);
+        }
+    }
+}
