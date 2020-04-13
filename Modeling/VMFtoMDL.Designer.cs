659e537a53028791d8f79ac546d221573d9329cf
jeanknapp <email+bitbucket@jeankn.app>
2020-04-13 00:05:51 -0400

    Changed the VMF to MDL UI to the new layout.

--- Modeling/VMFtoMDL.Designer.cs
+++ Modeling/VMFtoMDL.Designer.cs
@@ -34,9 +34,12 @@
             this.compileButton = new DevExpress.XtraEditors.SimpleButton();
             this.addButton = new DevExpress.XtraEditors.SimpleButton();
             this.removeButton = new DevExpress.XtraEditors.SimpleButton();
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
             ((System.ComponentModel.ISupportInitialize)(this.vmfListBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.panelControl3)).BeginInit();
             this.panelControl3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // vmfListBox
@@ -45,25 +48,24 @@
             this.vmfListBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.vmfListBox.Location = new System.Drawing.Point(8, 8);
             this.vmfListBox.Name = "vmfListBox";
-            this.vmfListBox.Size = new System.Drawing.Size(477, 239);
+            this.vmfListBox.Size = new System.Drawing.Size(534, 208);
             this.vmfListBox.TabIndex = 19;
             // 
             // panelControl3
             // 
             this.panelControl3.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
-            this.panelControl3.Controls.Add(this.compileButton);
             this.panelControl3.Controls.Add(this.addButton);
             this.panelControl3.Controls.Add(this.removeButton);
             this.panelControl3.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panelControl3.Location = new System.Drawing.Point(485, 8);
+            this.panelControl3.Location = new System.Drawing.Point(542, 8);
             this.panelControl3.Name = "panelControl3";
-            this.panelControl3.Size = new System.Drawing.Size(85, 239);
+            this.panelControl3.Size = new System.Drawing.Size(28, 208);
             this.panelControl3.TabIndex = 20;
             // 
             // compileButton
             // 
             this.compileButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.compileButton.Location = new System.Drawing.Point(10, 216);
+            this.compileButton.Location = new System.Drawing.Point(459, 7);
             this.compileButton.Name = "compileButton";
             this.compileButton.Size = new System.Drawing.Size(75, 23);
             this.compileButton.TabIndex = 20;
@@ -72,21 +74,35 @@
             // 
             // addButton
             // 
-            this.addButton.Location = new System.Drawing.Point(10, 0);
+            this.addButton.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("addButton.ImageOptions.SvgImage")));
+            this.addButton.ImageOptions.SvgImageSize = new System.Drawing.Size(16, 16);
+            this.addButton.Location = new System.Drawing.Point(5, 0);
             this.addButton.Name = "addButton";
-            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.Size = new System.Drawing.Size(23, 23);
             this.addButton.TabIndex = 18;
             this.addButton.Text = "Add";
             this.addButton.Click += new System.EventHandler(this.addButton_Click);
             // 
             // removeButton
             // 
-            this.removeButton.Location = new System.Drawing.Point(10, 29);
+            this.removeButton.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("removeButton.ImageOptions.SvgImage")));
+            this.removeButton.ImageOptions.SvgImageSize = new System.Drawing.Size(16, 16);
+            this.removeButton.Location = new System.Drawing.Point(5, 29);
             this.removeButton.Name = "removeButton";
-            this.removeButton.Size = new System.Drawing.Size(75, 23);
+            this.removeButton.Size = new System.Drawing.Size(23, 23);
             this.removeButton.TabIndex = 19;
             this.removeButton.Text = "Remove";
             this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.panelControl1.Controls.Add(this.compileButton);
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelControl1.Location = new System.Drawing.Point(8, 216);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(562, 31);
+            this.panelControl1.TabIndex = 21;
             // 
             // VMFtoMDL
             // 
@@ -95,6 +111,7 @@
             this.ClientSize = new System.Drawing.Size(578, 255);
             this.Controls.Add(this.vmfListBox);
             this.Controls.Add(this.panelControl3);
+            this.Controls.Add(this.panelControl1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "VMFtoMDL";
             this.Padding = new System.Windows.Forms.Padding(8);
@@ -102,6 +119,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.vmfListBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.panelControl3)).EndInit();
             this.panelControl3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -113,5 +132,6 @@
         private DevExpress.XtraEditors.SimpleButton compileButton;
         private DevExpress.XtraEditors.SimpleButton addButton;
         private DevExpress.XtraEditors.SimpleButton removeButton;
+        private DevExpress.XtraEditors.PanelControl panelControl1;
     }
 }
